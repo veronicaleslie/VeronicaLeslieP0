2f8b7f0ebfe1c58e70017e770f371f1aeb258a44
Veronica Leslie <v.leslie423@gmail.com>
2022-05-17 21:34:01 -0400

    made authservlet becuz i forgot :p

---
+++ src/main/java/com/revature/bankapp/web/servlets/AuthServlet.java
@@ -0,0 +1,47 @@
+package com.revature.bankapp.web.servlets;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.bankapp.exceptions.AuthenticationException;
+import com.revature.bankapp.exceptions.InvalidRequestException;
+import com.revature.bankapp.model.Users;
+import com.revature.bankapp.service.UserServices;
+import com.revature.bankapp.web.dto.LoginCreds;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class AuthServlet extends HttpServlet {
+
+    private final UserServices userServices;
+    // ObjectMapper provided by jackson
+    private final ObjectMapper mapper;
+
+    public AuthServlet(UserServices userServices, ObjectMapper mapper){
+        this.userServices = userServices;
+        this.mapper = mapper;
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        try {
+            LoginCreds loginCreds = mapper.readValue(req.getInputStream(), LoginCreds.class);
+
+            Users authUser = userServices.authenticateUser(loginCreds.getEmail(), loginCreds.getPassword());
+
+            HttpSession httpSession = req.getSession(true);
+            httpSession.setAttribute("authUser", authUser);
+
+            resp.getWriter().write("You have successfully logged in!");
+        } catch (AuthenticationException | InvalidRequestException e){
+            resp.setStatus(404);
+            resp.getWriter().write(e.getMessage());
+        } catch (Exception e){
+            resp.setStatus(409);
+            resp.getWriter().write(e.getMessage());
+        }
+    }
+
+}
